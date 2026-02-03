f28acc8fd48a679f35521274be57fc40563754a1
akankshaschavan <9529426742akansha@gmail.com>
2026-02-03 13:46:01 +0530

    initial

--- Customer/src/main/java/com/example/demo/controller/CustomerController.java
+++ Customer/src/main/java/com/example/demo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-
+@CrossOrigin(origins = "*")
 public class CustomerController {
 	
 	@Autowired
